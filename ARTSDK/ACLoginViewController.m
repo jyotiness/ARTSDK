d8630df87d51e427798c480952f303277f9d1797
Anuj Agarwal <anuj.agarwal@ness.com>
2014-04-03 13:34:43 +0530

    Test check-in

--- ARTSDK/ACLoginViewController.m
+++ ARTSDK/ACLoginViewController.m
@@ -250,6 +250,7 @@
              self.error =  ACLocalizedString(@"Your email address or password is incorrect", @"Your email address or password is incorrect");
              self.tableView.tableHeaderView = [self tableViewHeader];
              [self.tableView reloadData];
+             //
              
          }];
     } else {
